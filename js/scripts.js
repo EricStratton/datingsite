546b13c1110afe68af588d16f7ad649016d75167
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-22 14:52:15 -0700

    Add if functions to js.
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
-const brand = $("input:radio[name=fruit]:checked").val ();
+// const brand = $("input:radio[name=fruit]:checked").val ();
 
 // code to read value from animal selection //
-const animal = $("#animal").val();
+// const animal = $("#animal").val();
+
+$(document).ready(function () {
+  $('form#info').submit(function () {
+    const fruit = $("input:radio[name=fruit]:checked").val();
+    const animal = parseInt($("#animal").val());
+  
+    // const result = fruit + animal
+      
+    if (animal >=2 && fruit === "orange") {
+      $('#celebA').show();
+    } else if (animal <2 && >5 && fruit === "orange") {
+      $('#celebB').show();
+    } else (animal <=5 && fruit === "orange") {
+      $('#celebC').show();
+    }
+      
+  
+  
+  
+  
+  });
+});
